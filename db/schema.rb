aa39df9a68ea7051cd324b32b7216133997f7363
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-02-05 14:34:21 +0100

    reset migration & add controllers & models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170205195050) do
+ActiveRecord::Schema.define(version: 20170205195052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 20170205195050) do
     t.string   "content"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.datetime "read_at"
     t.index ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
     t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
