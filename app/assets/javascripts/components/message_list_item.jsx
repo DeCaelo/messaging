3ca249b8cb52c8db113d1828d7fc92aa2d25c908
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-02-07 11:21:32 +0100

    erb to jsx in message_list_item

--- app/assets/javascripts/components/message_list_item.jsx
+++ app/assets/javascripts/components/message_list_item.jsx
@@ -2,13 +2,15 @@ var MessageListItem = React.createClass({
   render: function() {
     return (
       <div className="message-container flexbox-start flex-item">
-        <span className="flex-item-shrink"><%= image_tag message.user.one_avatar_url, className: "avatar-square-small" %></span>
+        <span className="flex-item-shrink">
+          <img src={"TODO image_url"} className="avatar-square-small"/>
+        </span>
           <div className="message-content flex-item flexbox-columns">
             <ul className="message-header list-unstyled list-inline flex-item">
-              <li><h6><%= message.user.first_name %></h6></li>
-              <li className="date"><%= message.created_at.strftime("%b %e, %l:%M%P") %></li>
+              <li><h6>{"TODO FirstName"}</h6></li>
+              <li className="date">{"TODO date"}</li>
             </ul>
-          <%= simple_format message.content %>
+            <span dangerouslySetHTML={{"TODO content"}}</span>
         </div>
       </div>
     )
