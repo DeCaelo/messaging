1ae71e716752448ffc46ab7af17aa6a9d21a3124
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-02-07 11:09:28 +0100

    same for message

--- app/assets/javascripts/components/message_list.jsx
+++ app/assets/javascripts/components/message_list.jsx
@@ -2,18 +2,13 @@ var MessageList = React.createClass({
   render: function() {
     return(
       <div className="messages flexbox-columns-end flex-item">
-        <% @selected_conversation.messages.order(created_at: :asc).each do |message| %>
-          <div className="message-container flexbox-start flex-item">
-            <span className="flex-item-shrink"><%= image_tag message.user.one_avatar_url, className: "avatar-square-small" %></span>
-              <div className="message-content flex-item flexbox-columns">
-                <ul className="message-header list-unstyled list-inline flex-item">
-                  <li><h6><%= message.user.first_name %></h6></li>
-                  <li className="date"><%= message.created_at.strftime("%b %e, %l:%M%P") %></li>
-                </ul>
-              <%= simple_format message.content %>
-            </div>
-          </div>
-        <% end %>
+        {this.props.messages.map(function(message, index) {
+          return <MessageListItem
+                    message={message}
+                    key={index}
+                    // TODO props
+                    />
+        })}
       </div>
     )
   }
