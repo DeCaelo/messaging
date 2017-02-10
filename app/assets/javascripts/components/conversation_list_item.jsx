15b4c679164d1bca74980520652d12a2bd39c942
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-02-10 16:13:45 +0100

    fa & badge for checking conversation

--- app/assets/javascripts/components/conversation_list_item.jsx
+++ app/assets/javascripts/components/conversation_list_item.jsx
@@ -5,6 +5,13 @@ var ConversationListItem = React.createClass({
       "flexbox-start": true,
       "selected": this.props.conversation.id == this.props.selectedConversationId
     })
+    var iClasses = classNames({
+      "fa": this.props.conversation.is_last_message_writer_current_user,
+      "fa-reply": this.props.conversation.is_last_message_writer_current_user && !this.props.conversation.last_message_read_at,
+      "fa-check": this.props.conversation.is_last_message_writer_current_user && this.props.conversation.last_message_read_at,
+      "small-badge": !this.props.conversation.is_last_message_writer_current_user,
+      "small-badge-off": !this.props.conversation.is_last_message_writer_current_user && this.props.conversation.last_message_read_at
+    })
     return(
       <div className={divClasses} onClick={this.handleClick}>
         <span className="flex-item-shrink">
@@ -16,6 +23,7 @@ var ConversationListItem = React.createClass({
               <li className="date">{this.props.conversation.last_message_created_at}</li>
             </ul>
             <div className="message-preview flex-item">
+              <i className={iClasses}> </i>&nbsp;&nbsp;
               <span>{this.props.conversation.last_message_content}</span>
             </div>
           </div>
